712ff90db32da405632434665d61bd260ff1773a
Slava Krutelyov <slava77@cern.ch>
2014-05-19 14:17:48 -0500

    add sim, gmt, csctf makers and matching by dR

--- Phase2MuTriggerAnalysis/GMT2TTIntegration/python/ttgmtSequence_cff.py
+++ Phase2MuTriggerAnalysis/GMT2TTIntegration/python/ttgmtSequence_cff.py
@@ -1,5 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 from Phase2MuTriggerAnalysis.GMT2TTIntegration.ttMaker_cfi import *
+from Phase2MuTriggerAnalysis.GMT2TTIntegration.simtMaker_cfi import *
+from Phase2MuTriggerAnalysis.GMT2TTIntegration.gmtMaker_cfi import *
+from Phase2MuTriggerAnalysis.GMT2TTIntegration.csctfMaker_cfi import *
 
-ttgmtSequence = cms.Sequence(ttMaker)
+from Phase2MuTriggerAnalysis.GMT2TTIntegration.idxByDRMakers_cff import *
+
+ttgmtSequence = cms.Sequence(ttMaker + simtMaker + gmtMaker + csctfMaker + idxByDRMakers )
