31b3339288dd33ccfacac635ef6bd5f83f43aa12
Slava Krutelyov <slava77@cern.ch>
2014-05-29 07:51:12 -0500

    added leaf-cand makers for l1mu particles of different kind; non-cand part for them is still to be filled in a specific producer

--- Phase2MuTriggerAnalysis/GMT2TTIntegration/python/ttgmtSequence_cff.py
+++ Phase2MuTriggerAnalysis/GMT2TTIntegration/python/ttgmtSequence_cff.py
@@ -5,6 +5,10 @@ from Phase2MuTriggerAnalysis.GMT2TTIntegration.simtMaker_cfi import *
 from Phase2MuTriggerAnalysis.GMT2TTIntegration.gmtMaker_cfi import *
 from Phase2MuTriggerAnalysis.GMT2TTIntegration.csctfMaker_cfi import *
 
+from Phase2MuTriggerAnalysis.GMT2TTIntegration.l1muparticles_cff import *
+
 from Phase2MuTriggerAnalysis.GMT2TTIntegration.idxByDRMakers_cff import *
 
-ttgmtSequence = cms.Sequence(ttMaker + simtMaker + gmtMaker + csctfMaker + idxByDRMakers )
+
+
+ttgmtSequence = cms.Sequence(ttMaker + simtMaker + gmtMaker + csctfMaker + l1muMakers + idxByDRMakers )
