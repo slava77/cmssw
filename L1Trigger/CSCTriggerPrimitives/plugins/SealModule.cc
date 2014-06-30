5303fb3968ad186131fe39de7749045b78837d04
Sven Dildick <sven.dildick@cern.ch>
2014-06-30 22:04:55 +0200

    add class information

--- L1Trigger/CSCTriggerPrimitives/plugins/SealModule.cc
+++ L1Trigger/CSCTriggerPrimitives/plugins/SealModule.cc
@@ -1,5 +1,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include <L1Trigger/CSCTriggerPrimitives/plugins/CSCTriggerPrimitivesProducer.h>
-
 DEFINE_FWK_MODULE(CSCTriggerPrimitivesProducer);
+
+#include <L1Trigger/CSCTriggerPrimitives/plugins/CSCGoodTriggerDigisProducer.h>
+DEFINE_FWK_MODULE(CSCGoodTriggerDigisProducer);
--- L1Trigger/CSCTriggerPrimitives/python/cscGoodTriggerDigis_cfi.py
+++ L1Trigger/CSCTriggerPrimitives/python/cscGoodTriggerDigis_cfi.py
@@ -1,8 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 ## filter to study the performance of the CSC system when at random wiregroups, half-strips or TMBs fail
-cscGoodTriggerDigis = cms.EDProducer("CSCGoodTriggerDigisProducer",
+simCscGoodTriggerDigis = cms.EDProducer("CSCGoodTriggerDigisProducer",
     CSCComparatorDigiProducer = cms.InputTag("simMuonCSCDigis","MuonCSCComparatorDigi"),
     CSCWireDigiProducer = cms.InputTag("simMuonCSCDigis","MuonCSCWireDigi"),
+    ## on average how many bad half-strips per chamber
+    badStrips = cms.int32(0),
+    ## on average how many bad wiregroups per chamber
+    badWGs = cms.int32(0),
+    ## on average how many bad layers per chamber
+    badLayers = cms.int32(0),
 )
 
