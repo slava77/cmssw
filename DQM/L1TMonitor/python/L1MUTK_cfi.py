ceb12797980a656ee371ed14054d57a788811ced
Sergey Baranov <sergey.baranov@cern.ch>
2015-05-29 13:23:49 +0200

    interface/L1MUTK.h python/L1MUTK_cfi.py python/candMaker_cfi.py src/L1MUTK.cc src/L1MuTkCandMaker.cc  - have been added
    Conflicts:
    	DQM/L1TMonitor/interface/L1MUTK.h
    	DQM/L1TMonitor/python/L1MUTK_cfi.py
    	DQM/L1TMonitor/src/L1MUTK.cc

--- DQM/L1TMonitor/python/L1MUTK_cfi.py
+++ DQM/L1TMonitor/python/L1MUTK_cfi.py
@@ -4,13 +4,6 @@ l1MuTk = cms.EDAnalyzer("L1MUTK",
     disableROOToutput = cms.untracked.bool(True),
     verbose = cms.untracked.bool(False),
     gmtSource = cms.InputTag("gtDigis"),
-<<<<<<< HEAD
     DQMStore = cms.untracked.bool(True),
     candInputTag = cms.InputTag("L1TkMuonsMerge")
 )
-=======
-    DQMStore = cms.untracked.bool(True)
-)
-
-
->>>>>>> f1500e5... add interface/L1MUTK.h python/L1MUTK_cfi.py python/candMaker_cfi.py src/L1MUTK.cc src/L1MuTkCandMaker.cc
