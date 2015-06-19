9043ff41a89f6c5655a3f9d14cdc1dfd437418ff
Sergey Baranov <sergey.baranov@cern.ch>
2015-06-19 15:21:07 +0200

    rm

--- DQM/L1TMonitor/python/candMaker_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-candMaker = cms.EDProducer(
-    "L1MuTkCandMaker",
-    aliasPrefix = cms.untracked.string("cands"),
-    candInputTag = cms.InputTag("")
-    )
