7bf0335e94b46e7125ee4107087e096c832fbfbb
Sergey Baranov <sergey.baranov@cern.ch>
2015-07-06 13:23:09 +0200

    removed

--- DQM/L1TMonitor/python/L1MUTK_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-l1MuTk = cms.EDAnalyzer("L1MUTK",
-    disableROOToutput = cms.untracked.bool(True),
-    verbose = cms.untracked.bool(False),
-    gmtSource = cms.InputTag("gtDigis"),
-    DQMStore = cms.untracked.bool(True),
-    candInputTag = cms.InputTag("L1TkMuonsMerge")
-)
