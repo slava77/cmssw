e66b53f0afda3b38128297425d0c2808fadb43c5
Lindsey Gray <lindsey.gray@gmail.com>
2015-10-12 14:42:47 +0200

    add in 4D cov-matrices

--- DataFormats/GeometryCommonDetAlgo/interface/GlobalError.h
+++ DataFormats/GeometryCommonDetAlgo/interface/GlobalError.h
@@ -2,14 +2,17 @@
 #define _TRACKER_GLOBAL_ERROR_H_
 
 #include "DataFormats/GeometryCommonDetAlgo/interface/GlobalErrorBase.h"
+#include "DataFormats/GeometryCommonDetAlgo/interface/GlobalErrorBase4D.h"
 #include "DataFormats/GeometryCommonDetAlgo/interface/GlobalErrorBaseExtended.h"
 #include "DataFormats/GeometryCommonDetAlgo/interface/ErrorMatrixTag.h"
 
   /**
    * typedef to a  GlobalErrorBase object defined as a 3*3 covariance matrix
+   * typedef to a  GlobalErrorBaseExtended object defined as a 4*4 covariance matrix
    */
 
 typedef GlobalErrorBase<double,ErrorMatrixTag> GlobalError;
+typedef GlobalErrorBase4D<double,ErrorMatrixTag, GlobalError4D;
 typedef GlobalErrorBaseExtended<double,ErrorMatrixTag> GlobalErrorExtended;
 
 #endif
