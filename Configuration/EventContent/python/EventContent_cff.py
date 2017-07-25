6a1cd85430c887e0eb5320d0716ced2c54294f3c
perrozzi <perrozzi@cern.ch>
2017-07-25 09:22:20 +0200

    rename MINIAODGEN event content to MINIGEN

--- Configuration/EventContent/python/EventContent_cff.py
+++ Configuration/EventContent/python/EventContent_cff.py
@@ -781,7 +781,7 @@ MINIAODSIMEventContent= cms.PSet(
     compressionLevel=cms.untracked.int32(4)
 )
 
-MINIAODGENEventContent= cms.PSet(    
+MINIGENEventContent= cms.PSet(    
     outputCommands = cms.untracked.vstring('drop *'),
     eventAutoFlushCompressedSize=cms.untracked.int32(15*1024*1024),
     compressionAlgorithm=cms.untracked.string("LZMA"),
@@ -792,7 +792,7 @@ from PhysicsTools.PatAlgos.slimming.slimming_cff import MicroEventContent,MicroE
 
 MINIAODEventContent.outputCommands.extend(MicroEventContent.outputCommands)
 MINIAODSIMEventContent.outputCommands.extend(MicroEventContentMC.outputCommands)
-MINIAODGENEventContent.outputCommands.extend(MicroEventContentGEN.outputCommands)
+MINIGENEventContent.outputCommands.extend(MicroEventContentGEN.outputCommands)
 
 #### RAW+miniAOD
 
