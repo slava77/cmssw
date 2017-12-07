dd815f80e10ba421100f98a1d2f2d62e800cd1a7
Jay <jlawhorn@mit.edu>
2017-12-07 18:01:36 +0100

    initial commit for mahi

--- HLTrigger/Configuration/python/customizeHLTforCMSSW.py
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -17,6 +17,13 @@ from HLTrigger.Configuration.common import *
 #                     pset.minGoodStripCharge = cms.PSet(refToPSet_ = cms.string('HLTSiStripClusterChargeCutNone'))
 #     return process
 
+# Add mahi to HCAL local reconstruction 
+def customiseForXXXXX(process):
+    from RecoLocalCalo.HcalRecProducers.HBHEPhase1Reconstructor_cfi import hbheprereco
+    for producer in producers_by_type(process, "HBHEPhase1Reconstructor"):
+        producer.algorithm.useMahi = hbheprereco.algorithm.useMahi
+    return process
+
 # Add new parameters to RecoTrackRefSelector
 def customiseFor19029(process):
     for producer in producers_by_type(process, "RecoTrackRefSelector"):
@@ -64,5 +71,6 @@ def customizeHLTforCMSSW(process, menuType="GRun"):
     process = customiseFor20269(process)
     process = customiseFor19989(process)
     process = customiseFor20429(process)
-
+    process = customiseForXXXXX(process)
+    
     return process
