9631e2807125b4ec33f962728d4f7c7e781d2dc9
hajohajo@helsinki.fi <hajohajo@helsinki.fi>
2017-12-15 10:24:09 +0100

    Changed the track classifiers in iterative track reconstruction for Phase1 to use a Deep Neural Network implemented in the LWTNN package instead of Boosted Decision Trees

--- RecoTracker/IterativeTracking/python/DetachedQuadStep_cff.py
+++ RecoTracker/IterativeTracking/python/DetachedQuadStep_cff.py
@@ -171,11 +171,11 @@ detachedQuadStepTracks = RecoTracker.TrackProducer.TrackProducer_cfi.TrackProduc
 )
 
 # TRACK SELECTION AND QUALITY FLAG SETTING.
-from RecoTracker.FinalTrackSelectors.TrackMVAClassifierDetached_cfi import *
-detachedQuadStep = TrackMVAClassifierDetached.clone(
+from RecoTracker.FinalTrackSelectors.TrackLwtnnClassifier_cfi import *
+from RecoTracker.FinalTrackSelectors.trackSelectionLwtnn_cfi import *
+detachedQuadStep = TrackLwtnnClassifier.clone(
     src = 'detachedQuadStepTracks',
-    mva = dict(GBRForestLabel = 'MVASelectorDetachedQuadStep_Phase1'),
-    qualityCuts = [-0.5,0.0,0.5],
+    qualityCuts = [-0.6,0.1,0.85],
 )
 
 
