b780fc3c04590be3aae0efb7e6c9711381692ebf
hajohajo@helsinki.fi <hajohajo@helsinki.fi>
2018-02-16 07:24:14 +0100

    Change to training v7

--- RecoTracker/FinalTrackSelectors/python/trackSelectionLwtnn_cfi.py
+++ RecoTracker/FinalTrackSelectors/python/trackSelectionLwtnn_cfi.py
@@ -1,5 +1,5 @@
 from RecoTracker.FinalTrackSelectors.lwtnnESProducer_cfi import lwtnnESProducer as _lwtnnESProducer
 trackSelectionLwtnn = _lwtnnESProducer.clone(
     ComponentName = "trackSelectionLwtnn",
-    fileName = "RecoTracker/FinalTrackSelectors/data/LWTNN_network_v3.json",
+    fileName = "RecoTracker/FinalTrackSelectors/data/LWTNN_network_v7.json",
 )
