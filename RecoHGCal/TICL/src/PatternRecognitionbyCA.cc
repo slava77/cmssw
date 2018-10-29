d67ba7969e0874e6b47f297dcde4e879f326ce42
Felice <felice.pantaleo@cern.ch>
2018-10-29 18:31:49 +0100

    adding pattern recognition by Cellular Automaton

---
+++ RecoHGCal/TICL/src/PatternRecognitionbyCA.cc
@@ -0,0 +1,14 @@
+#include "RecoHGCal/TICL/interface/PatternRecognitionbyCA.h"
+
+
+
+void PatternRecognitionbyCA::makeTracksters(
+      const edm::Event& ev,
+      const edm::EventSetup& es,
+      const std::vector<reco::CaloCluster>& layerClusters,
+      const std::vector<std::pair<unsigned int, float> >& mask, std::vector<Trackster>& result) const
+      {
+
+
+          std::cout << "making Tracksters" << std::endl;
+      }
