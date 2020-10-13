1e2e7000f807e4f151d84d79783ac861ed6e3339
bainbrid <bainbrid@users.noreply.github.com>
2020-10-13 17:32:07 +0200

    rekey seed BDT value maps and add to electronIDSources; run and add lowPtGsfLinks to userCands; embed collections; add miniIso

--- PhysicsTools/PatAlgos/python/producersLayer1/lowPtElectronProducer_cff.py
+++ PhysicsTools/PatAlgos/python/producersLayer1/lowPtElectronProducer_cff.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from PhysicsTools.PatAlgos.mcMatchLayer0.electronMatch_cfi import *
 from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
 from PhysicsTools.PatAlgos.producersLayer1.electronProducer_cfi import *
+from PhysicsTools.PatAlgos.slimming.lowPtGsfLinks_cfi import lowPtGsfLinks
 
 sourceElectrons = cms.InputTag("lowPtGsfElectrons")
 
@@ -11,42 +12,80 @@ lowPtElectronMatch = electronMatch.clone(
 )
 
 patLowPtElectrons = patElectrons.clone(
-    # input collections
+
+    # Input collection
     electronSource = sourceElectrons,
+
+    # MC matching
+    addGenMatch      = cms.bool(True),
+    embedGenMatch    = cms.bool(True),
     genParticleMatch = cms.InputTag("lowPtElectronMatch"),
-    # overrides
-    addElectronID = cms.bool(False),
-    addGenMatch = cms.bool(True),
-    addMVAVariables = cms.bool(False),
-    addPFClusterIso = cms.bool(False),
-    electronIDSources = cms.PSet(),
-    computeMiniIso = cms.bool(False),
-    isoDeposits = cms.PSet(),
-    isolationValues = cms.PSet(),
-    isolationValuesNoPFId = cms.PSet(),
-    miniIsoParamsB = cms.vdouble(),
-    miniIsoParamsE = cms.vdouble(),
-    usePfCandidateMultiMap = cms.bool(False),
-    # embedding
-    embedBasicClusters          = cms.bool(False),
-    embedGenMatch               = cms.bool(False),
-    embedGsfElectronCore        = cms.bool(False),
-    embedGsfTrack               = cms.bool(False),
-    embedHighLevelSelection     = cms.bool(False),
+
+    # Electron ID
+    addElectronID = cms.bool(True),
+    electronIDSources = cms.PSet(
+        unbiased = cms.InputTag("rekeyLowPtGsfElectronSeedValueMaps:unbiased"),
+        ptbiased = cms.InputTag("rekeyLowPtGsfElectronSeedValueMaps:ptbiased"),
+    ),
+
+    # User data to add
+    userData = cms.PSet(
+        userClasses = cms.PSet( src = cms.VInputTag('') ),
+        userFloats = cms.PSet( src = cms.VInputTag('') ),
+        userInts = cms.PSet( src = cms.VInputTag('') ),
+        userCands = cms.PSet( src = cms.VInputTag('lowPtGsfLinks') ),
+        userFunctions = cms.vstring(),
+        userFunctionLabels = cms.vstring()
+    ),
+
+    # Embedding of RECO/AOD items
+    embedGsfElectronCore        = cms.bool(True),
     embedPFCandidate            = cms.bool(False),
-    embedPflowBasicClusters     = cms.bool(False),
-    embedPflowPreshowerClusters = cms.bool(False),
-    embedPflowSuperCluster      = cms.bool(False),
+    embedGsfTrack               = cms.bool(True),
+    embedTrack                  = cms.bool(True),
+    embedSuperCluster           = cms.bool(True),
+    embedSeedCluster            = cms.bool(True),
+    embedBasicClusters          = cms.bool(True),
     embedPreshowerClusters      = cms.bool(False),
     embedRecHits                = cms.bool(False),
-    embedSeedCluster            = cms.bool(False),
-    embedSuperCluster           = cms.bool(False),
-    embedTrack                  = cms.bool(True),
-    )
+    embedPflowSuperCluster      = cms.bool(False),
+    embedPflowBasicClusters     = cms.bool(False),
+    embedPflowPreshowerClusters = cms.bool(False),
+
+    # Mini-Isolation 
+    computeMiniIso    = cms.bool(True), # See: PhysicsTools/PatUtils/src/MiniIsolation.cc
+    pfCandsForMiniIso = cms.InputTag("packedPFCandidates"),
+    miniIsoParamsE    = cms.vdouble(0.2, 0.2, 10.0, 0.0, 0.015, 0.015, 0.08, 0.0, 0.0),
+    miniIsoParamsB    = cms.vdouble(0.2, 0.2, 10.0, 0.0, 0.000, 0.000, 0.00, 0.0, 0.0),
+
+    # Miscellaneous flags
+    useParticleFlow         = cms.bool(False),
+    usePfCandidateMultiMap  = cms.bool(False),
+    addMVAVariables         = cms.bool(False),
+    addEfficiencies         = cms.bool(False),
+    addResolutions          = cms.bool(False),
+    embedHighLevelSelection = cms.bool(False),
+    addPFClusterIso         = cms.bool(False),
+    addPuppiIsolation       = cms.bool(False),
+    isoDeposits             = cms.PSet(),
+    userIsolation           = cms.PSet(),
+    isolationValues         = cms.PSet(),
+    isolationValuesNoPFId   = cms.PSet(),
+)
+
+from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronSeedValueMaps_cfi import lowPtGsfElectronSeedValueMaps
+rekeyLowPtGsfElectronSeedValueMaps = lowPtGsfElectronSeedValueMaps.clone(
+    Rekey=cms.bool(True),
+    gsfElectrons=sourceElectrons,
+    floatValueMaps=cms.VInputTag(["lowPtGsfElectronSeedValueMaps:unbiased",
+                                  "lowPtGsfElectronSeedValueMaps:ptbiased"]),
+)
 
 makePatLowPtElectronsTask = cms.Task(
+    lowPtGsfLinks,
     lowPtElectronMatch,
-    patLowPtElectrons
+    rekeyLowPtGsfElectronSeedValueMaps,
+    patLowPtElectrons,
     )
 
 makePatLowPtElectrons = cms.Sequence(makePatLowPtElectronsTask)
@@ -61,3 +100,21 @@ from Configuration.Eras.Modifier_run2_miniAOD_94XFall17_cff import run2_miniAOD_
                                                            electronSource = "gedGsfElectrons",
                                                            genParticleMatch = "electronMatch"
                                                            )
+
+# Rekey seed BDT ValueMaps by reco::GsfElectron and add to electronIDSources, only for run2_miniAOD_UL
+#from Configuration.ProcessModifiers.run2_miniAOD_UL_cff import run2_miniAOD_UL
+#from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronSeedValueMaps_cfi import lowPtGsfElectronSeedValueMaps
+#rekeyLowPtGsfElectronSeedValueMaps = lowPtGsfElectronSeedValueMaps.clone(
+#    Rekey=cms.bool(True),
+#    gsfElectrons=sourceElectrons,
+#    floatValueMaps=cms.VInputTag(["lowPtGsfElectronSeedValueMaps:unbiased",
+#                                  "lowPtGsfElectronSeedValueMaps:ptbiased"]),
+#)
+#makePatLowPtElectronsTask_ = cms.Task(makePatLowPtElectronsTask.copy(),rekeyLowPtGsfElectronSeedValueMaps)
+#run2_miniAOD_UL.toReplaceWith(makePatLowPtElectronsTask,makePatLowPtElectronsTask_)
+#run2_miniAOD_UL.toModify(patLowPtElectrons,
+#                         electronIDSources = cms.PSet(
+#                             unbiased = cms.InputTag("rekeyLowPtGsfElectronSeedValueMaps:unbiased"),
+#                             ptbiased = cms.InputTag("rekeyLowPtGsfElectronSeedValueMaps:ptbiased"),
+#                         ),
+#                     )
