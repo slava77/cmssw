d36d55b6b501b3183da4df16fabc1ad55a9889c3
bainbrid <bainbrid@users.noreply.github.com>
2020-10-14 20:33:23 +0200

    scram build code-format

--- CommonTools/MVAUtils/bin/convertXMLToGBRForestROOT.cc
+++ CommonTools/MVAUtils/bin/convertXMLToGBRForestROOT.cc
@@ -10,25 +10,25 @@ int main(int argc, char **argv) {
     std::cout << "Please pass a (gzipped) BDT weight file and a name for the output ROOT file." << std::endl;
     return 1;
   }
-  
+
   char *inputFileName = argv[1];
   char *outputFileName = argv[2];
-  
+
   if (!boost::filesystem::exists(inputFileName)) {
     std::cout << "Input file " << inputFileName << " does not exists." << std::endl;
     return 1;
   }
-  
+
   if (boost::filesystem::exists(outputFileName)) {
     std::cout << "Output file " << outputFileName << " already exists." << std::endl;
     return 1;
   }
-  
+
   auto gbrForest = createGBRForest(inputFileName);
   std::cout << "Read GBRForest " << inputFileName << " successfully." << std::endl;
-  
+
   TFile{outputFileName, "RECREATE"}.WriteObject(gbrForest.get(), "gbrForest");
   std::cout << "GBRForest written to " << outputFileName << " successfully." << std::endl;
-  
+
   return 0;
 }
