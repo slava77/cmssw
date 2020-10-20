4cbfcd8b75cee3109a046c06f33329a35ce12416
bainbrid <bainbrid@users.noreply.github.com>
2020-10-20 22:31:34 +0200

    add patLowPtGsfElectronID to default PAT sequence, plus tidy up

--- PhysicsTools/PatAlgos/python/slimming/slimmedLowPtElectrons_cff.py
+++ PhysicsTools/PatAlgos/python/slimming/slimmedLowPtElectrons_cff.py
@@ -1,31 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-# Clone with config for userFloat("ID")
 from PhysicsTools.PatAlgos.slimming.slimmedLowPtElectrons_cfi import slimmedLowPtElectrons
-slimmedLowPtElectrons_ = slimmedLowPtElectrons.clone(
-    modifyElectrons = cms.bool(True),
-    modifierConfig = cms.PSet(
-        modifications = cms.VPSet(
-            cms.PSet(
-                electron_config = cms.PSet(
-                    ID = cms.InputTag("patLowPtGsfElectronID"),
-                    electronSrc = cms.InputTag("patLowPtElectrons"),
-                ),
-                modifierName = cms.string('EGExtraInfoModifierFromFloatValueMaps'),
-                photon_config = cms.PSet()
-            ),
-        )
-    )
-)
-
-# Add userFloat("ID") only for run2_miniAOD_UL
-from Configuration.ProcessModifiers.run2_miniAOD_UL_cff import run2_miniAOD_UL
-run2_miniAOD_UL.toReplaceWith(slimmedLowPtElectrons,slimmedLowPtElectrons_)
-
-# Task
-slimmedLowPtElectronsTask = cms.Task(
-    slimmedLowPtElectrons
-)
 
 # Clone to run ID V2 on patLowPtElectrons
 from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronID_cfi import lowPtGsfElectronID
@@ -36,6 +11,8 @@ patLowPtGsfElectronID = lowPtGsfElectronID.clone(
     Version=cms.string('V1'),
 )
 
-# Add ID module to task only for run2_miniAOD_UL
-slimmedLowPtElectronsTask_ = cms.Task(slimmedLowPtElectronsTask.copy(),patLowPtGsfElectronID)
-run2_miniAOD_UL.toReplaceWith(slimmedLowPtElectronsTask,slimmedLowPtElectronsTask_)
+# Task
+slimmedLowPtElectronsTask = cms.Task(
+    slimmedLowPtElectrons,
+    patLowPtGsfElectronID
+)
