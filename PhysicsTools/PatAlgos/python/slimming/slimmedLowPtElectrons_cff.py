bd9e67dd80ec7ac2cef3acce0b2abcf956e1d2d8
bainbrid <bainbrid@users.noreply.github.com>
2020-10-22 11:40:00 +0200

    add back lowPtGsfLinks to slimming step

--- PhysicsTools/PatAlgos/python/slimming/slimmedLowPtElectrons_cff.py
+++ PhysicsTools/PatAlgos/python/slimming/slimmedLowPtElectrons_cff.py
@@ -1,9 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 from PhysicsTools.PatAlgos.slimming.slimmedLowPtElectrons_cfi import slimmedLowPtElectrons
+from PhysicsTools.PatAlgos.slimming.lowPtGsfLinks_cfi import lowPtGsfLinks
+from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronID_cfi import lowPtGsfElectronID
 
 # Clone to run ID V2 on patLowPtElectrons
-from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronID_cfi import lowPtGsfElectronID
 patLowPtGsfElectronID = lowPtGsfElectronID.clone(
     electrons='patLowPtElectrons',
     rho='fixedGridRhoFastjetAll',
@@ -13,6 +14,7 @@ patLowPtGsfElectronID = lowPtGsfElectronID.clone(
 
 # Task
 slimmedLowPtElectronsTask = cms.Task(
+    lowPtGsfLinks,
+    patLowPtGsfElectronID,
     slimmedLowPtElectrons,
-    patLowPtGsfElectronID
 )
