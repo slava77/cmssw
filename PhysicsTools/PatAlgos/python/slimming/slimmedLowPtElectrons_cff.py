965d82a43d85fea0b4a2b8ee604f054579580c76
bainbrid <bainbrid@users.noreply.github.com>
2020-11-06 21:22:17 +0100

    IDProducer in slimming step for bParking (commented)

--- PhysicsTools/PatAlgos/python/slimming/slimmedLowPtElectrons_cff.py
+++ PhysicsTools/PatAlgos/python/slimming/slimmedLowPtElectrons_cff.py
@@ -4,17 +4,35 @@ from PhysicsTools.PatAlgos.slimming.slimmedLowPtElectrons_cfi import slimmedLowP
 from PhysicsTools.PatAlgos.slimming.lowPtGsfLinks_cfi import lowPtGsfLinks
 from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronID_cfi import lowPtGsfElectronID
 
-# Clone to run ID V2 on patLowPtElectrons
-patLowPtGsfElectronID = lowPtGsfElectronID.clone(
-    electrons='patLowPtElectrons',
-    rho='fixedGridRhoFastjetAll',
-    ModelWeights=cms.vstring(['RecoEgamma/ElectronIdentification/data/LowPtElectrons/LowPtElectrons_ID_2020Sept15.root']),
-    Version=cms.string('V1'),
-)
-
 # Task
 slimmedLowPtElectronsTask = cms.Task(
     lowPtGsfLinks,
-    patLowPtGsfElectronID,
     slimmedLowPtElectrons,
 )
+
+# Modifiers for BParking
+#from Configuration.Eras.Modifier_bParking_cff import bParking
+#
+#patLowPtGsfElectronID = lowPtGsfElectronID.clone(
+#    electrons=cms.InputTag('patLowPtElectrons'),
+#    rho=cms.InputTag('fixedGridRhoFastjetAll'),
+#    unbiased=cms.InputTag(''), # instead, use embedded value from userData('unbiased')
+#    ModelWeights=cms.vstring(['RecoEgamma/ElectronIdentification/data/LowPtElectrons/LowPtElectrons_ID_2020Sept15.root']),
+#    Version=cms.string('V1'),
+#)
+#slimmedLowPtElectronsTask_ = slimmedLowPtElectronsTask.copy()
+#slimmedLowPtElectronsTask_.add(patLowPtGsfElectronID)
+#bParking.toReplaceWith(slimmedLowPtElectronsTask,slimmedLowPtElectronsTask_)
+#
+#slimmedLowPtElectrons_ = slimmedLowPtElectrons.clone()
+#slimmedLowPtElectrons_.modifierConfig.modifications.append(
+#    cms.PSet(
+#        electron_config = cms.PSet(
+#            ID = cms.InputTag("patLowPtGsfElectronID"),
+#            electronSrc = cms.InputTag("patLowPtElectrons"),
+#        ),
+#        modifierName = cms.string('EGExtraInfoModifierFromFloatValueMaps'),
+#        photon_config = cms.PSet()
+#    )
+#)
+#bParking.toReplaceWith(slimmedLowPtElectrons,slimmedLowPtElectrons_)
