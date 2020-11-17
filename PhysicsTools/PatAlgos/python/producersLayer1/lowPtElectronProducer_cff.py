7f6d4ae3482e1c1f61928cc4703e6303e51b1d9c
bainbrid <bainbrid@users.noreply.github.com>
2020-11-17 17:13:20 +0100

    slava: clean up

--- PhysicsTools/PatAlgos/python/producersLayer1/lowPtElectronProducer_cff.py
+++ PhysicsTools/PatAlgos/python/producersLayer1/lowPtElectronProducer_cff.py
@@ -16,56 +16,39 @@ patLowPtElectrons = patElectrons.clone(
     electronSource = sourceElectrons,
 
     # MC matching
-    addGenMatch      = cms.bool(True),
-    embedGenMatch    = cms.bool(False),
-    genParticleMatch = cms.InputTag("lowPtElectronMatch"),
+    genParticleMatch = "lowPtElectronMatch",
 
     # Electron ID
-    addElectronID = cms.bool(True),
-    electronIDSources = cms.PSet(
+    addElectronID = True,
+    electronIDSources = dict(
         unbiased = cms.InputTag("rekeyLowPtGsfElectronSeedValueMaps:unbiased"),
         ptbiased = cms.InputTag("rekeyLowPtGsfElectronSeedValueMaps:ptbiased"),
         ID       = cms.InputTag("lowPtGsfElectronID"),
     ),
 
-    # User data to add
-    userData = cms.PSet(
-        userClasses = cms.PSet( src = cms.VInputTag('') ),
-        userFloats = cms.PSet( src = cms.VInputTag('') ),
-        userInts = cms.PSet( src = cms.VInputTag('') ),
-        userCands = cms.PSet( src = cms.VInputTag('') ),
-        userFunctions = cms.vstring(),
-        userFunctionLabels = cms.vstring()
-    ),
+    # Embedding of RECO/AOD items
 
     # Embedding of RECO/AOD items
-    embedTrack                  = cms.bool(True),
-    embedGsfElectronCore        = cms.bool(True),
-    embedGsfTrack               = cms.bool(True),
-    embedSuperCluster           = cms.bool(True),
-    embedSeedCluster            = cms.bool(True),
-    embedBasicClusters          = cms.bool(True),
-    embedPreshowerClusters      = cms.bool(False),
-    embedRecHits                = cms.bool(False),
-    embedPflowSuperCluster      = cms.bool(False),
-    embedPflowBasicClusters     = cms.bool(False),
-    embedPflowPreshowerClusters = cms.bool(False),
-    embedPFCandidate            = cms.bool(False),
+    embedTrack                  = True,
+    embedGsfElectronCore        = True,
+    embedGsfTrack               = True,
+    embedSuperCluster           = True,
+    embedSeedCluster            = True,
+    embedBasicClusters          = True,
+    embedPreshowerClusters      = False,
+    embedRecHits                = False,
+    embedPflowSuperCluster      = False,
+    embedPflowBasicClusters     = False,
+    embedPflowPreshowerClusters = False,
+    embedPFCandidate            = False,
 
     # Miscellaneous flags
-    useParticleFlow         = cms.bool(False),
-    usePfCandidateMultiMap  = cms.bool(False),
-    addMVAVariables         = cms.bool(False),
-    addEfficiencies         = cms.bool(False),
-    addResolutions          = cms.bool(False),
-    embedHighLevelSelection = cms.bool(False),
-    computeMiniIso          = cms.bool(False),
-    addPFClusterIso         = cms.bool(False),
-    addPuppiIsolation       = cms.bool(False),
+    addMVAVariables         = False,
+    embedHighLevelSelection = False,
     isoDeposits             = cms.PSet(),
-    userIsolation           = cms.PSet(),
     isolationValues         = cms.PSet(),
     isolationValuesNoPFId   = cms.PSet(),
+
 )
 
 makePatLowPtElectronsTask = cms.Task(
