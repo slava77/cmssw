bce33b9b846e09f5b38a049fe127d15e07725824
bainbrid <bainbrid@users.noreply.github.com>
2020-11-17 17:28:18 +0100

    slava: removed commented python

--- PhysicsTools/PatAlgos/python/slimming/slimmedLowPtElectrons_cff.py
+++ PhysicsTools/PatAlgos/python/slimming/slimmedLowPtElectrons_cff.py
@@ -9,30 +9,3 @@ slimmedLowPtElectronsTask = cms.Task(
     lowPtGsfLinks,
     slimmedLowPtElectrons,
 )
-
-# Modifiers for BParking
-#from Configuration.Eras.Modifier_bParking_cff import bParking
-#
-#patLowPtGsfElectronID = lowPtGsfElectronID.clone(
-#    electrons=cms.InputTag('patLowPtElectrons'),
-#    rho=cms.InputTag('fixedGridRhoFastjetAll'),
-#    unbiased=cms.InputTag(''), # instead, use embedded value from userData('unbiased')
-#    ModelWeights=cms.vstring(['RecoEgamma/ElectronIdentification/data/LowPtElectrons/LowPtElectrons_ID_2020Sept15.root']),
-#    Version=cms.string('V1'),
-#)
-#slimmedLowPtElectronsTask_ = slimmedLowPtElectronsTask.copy()
-#slimmedLowPtElectronsTask_.add(patLowPtGsfElectronID)
-#bParking.toReplaceWith(slimmedLowPtElectronsTask,slimmedLowPtElectronsTask_)
-#
-#slimmedLowPtElectrons_ = slimmedLowPtElectrons.clone()
-#slimmedLowPtElectrons_.modifierConfig.modifications.append(
-#    cms.PSet(
-#        electron_config = cms.PSet(
-#            ID = cms.InputTag("patLowPtGsfElectronID"),
-#            electronSrc = cms.InputTag("patLowPtElectrons"),
-#        ),
-#        modifierName = cms.string('EGExtraInfoModifierFromFloatValueMaps'),
-#        photon_config = cms.PSet()
-#    )
-#)
-#bParking.toReplaceWith(slimmedLowPtElectrons,slimmedLowPtElectrons_)
