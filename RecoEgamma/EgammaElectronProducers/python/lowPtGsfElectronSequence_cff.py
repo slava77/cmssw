40dc940a8f00d1a462eb6193fb75b671bb3df21c
bainbrid <bainbrid@users.noreply.github.com>
2020-12-21 18:46:50 +0100

    slava's review

--- RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronSequence_cff.py
+++ RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectronSequence_cff.py
@@ -63,8 +63,8 @@ from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronSuperClusters_cff import
 from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronCores_cff import *
 
 # Low pT electrons
+from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronsPreRegression_cfi import *
 from RecoEgamma.EgammaElectronProducers.lowPtGsfElectrons_cfi import *
-from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronFinalizer_cfi import *
 
 # Low pT Electron value maps
 from RecoEgamma.EgammaElectronProducers.lowPtGsfElectronSeedValueMaps_cff import lowPtGsfElectronSeedValueMaps
--- RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectrons_cfi.py
+++ RecoEgamma/EgammaElectronProducers/python/lowPtGsfElectrons_cfi.py
@@ -68,3 +68,53 @@ fastSim.toModify(lowPtGsfElectrons,ctfTracksTag = cms.InputTag("generalTracksBef
 from Configuration.Eras.Modifier_pp_on_AA_2018_cff import pp_on_AA_2018
 pp_on_AA_2018.toModify(lowPtGsfElectrons.preselection, minSCEtBarrel = 15.0)
 pp_on_AA_2018.toModify(lowPtGsfElectrons.preselection, minSCEtEndcaps = 15.0)
+
+################################################################################
+# "lowPtGsfElectrons" (LowPtGsfElectronProducer) above is run as part of RECO
+# "lowPtGsfElectrons" (LowPtGsfElectronFinalizer) below is part of run2_miniAOD_UL
+
+from RecoEgamma.EgammaTools.regressionModifier_cfi import regressionModifier106XUL
+_lowPtRegressionModifier = regressionModifier106XUL.clone(
+    modifierName = 'EGRegressionModifierV3',
+    rhoTag = 'fixedGridRhoFastjetAll',
+    eleRegs = dict(
+        ecalOnlyMean = dict(
+            lowEtHighEtBoundary = 20.,
+            ebLowEtForestName = ":lowPtElectron_eb_ecalOnly_05To50_mean",
+            ebHighEtForestName = ":lowPtElectron_eb_ecalOnly_05To50_mean",
+            eeLowEtForestName = ":lowPtElectron_ee_ecalOnly_05To50_mean",
+            eeHighEtForestName = ":lowPtElectron_ee_ecalOnly_05To50_mean",
+            ),
+        ecalOnlySigma = dict(
+            lowEtHighEtBoundary = 20.,
+            ebLowEtForestName = ":lowPtElectron_eb_ecalOnly_05To50_sigma",
+            ebHighEtForestName = ":lowPtElectron_eb_ecalOnly_05To50_sigma",
+            eeLowEtForestName = ":lowPtElectron_ee_ecalOnly_05To50_sigma",
+            eeHighEtForestName = ":lowPtElectron_ee_ecalOnly_05To50_sigma",
+            ),
+        epComb = dict(
+            ecalTrkRegressionConfig = dict(
+                lowEtHighEtBoundary = 20.,
+                ebLowEtForestName = ":lowPtElectron_eb_ecalTrk_05To50_mean",
+                ebHighEtForestName = ":lowPtElectron_eb_ecalTrk_05To50_mean",
+                eeLowEtForestName = ":lowPtElectron_ee_ecalTrk_05To50_mean",
+                eeHighEtForestName = ":lowPtElectron_ee_ecalTrk_05To50_mean",
+                ),
+            ecalTrkRegressionUncertConfig = dict(
+                lowEtHighEtBoundary = 20.,
+                ebLowEtForestName = ":lowPtElectron_eb_ecalTrk_05To50_sigma",
+                ebHighEtForestName = ":lowPtElectron_eb_ecalTrk_05To50_sigma",
+                eeLowEtForestName = ":lowPtElectron_ee_ecalTrk_05To50_sigma",
+                eeHighEtForestName = ":lowPtElectron_ee_ecalTrk_05To50_sigma",
+                ),
+        )
+    ),
+)
+
+_lowPtGsfElectrons = cms.EDProducer("LowPtGsfElectronFinalizer",
+                                    previousGsfElectronsTag = cms.InputTag("lowPtGsfElectrons::@skipCurrentProcess"),
+                                    regressionConfig = _lowPtRegressionModifier,
+)
+
+from Configuration.ProcessModifiers.run2_miniAOD_UL_cff import run2_miniAOD_UL
+run2_miniAOD_UL.toReplaceWith(lowPtGsfElectrons,_lowPtGsfElectrons)
