d363863354e76d66bfae3bea003e0df603890ef5
Angela Czirkos <czirkosangela@gmail.com>
2021-06-30 17:58:39 +0200

    add missing header include

--- CUDADataFormats/TrackingRecHit/interface/SiPixelStatus.h
+++ CUDADataFormats/TrackingRecHit/interface/SiPixelStatus.h
@@ -1,6 +1,8 @@
 #ifndef CUDADataFormats_TrackingRecHit_interface_SiPixelStatus_H
 #define CUDADataFormats_TrackingRecHit_interface_SiPixelStatus_H
 
+#include <cstdint>
+
 // more information on bit fields : https://en.cppreference.com/w/cpp/language/bit_field
 struct SiPixelStatus {
   uint8_t isBigX : 1;
