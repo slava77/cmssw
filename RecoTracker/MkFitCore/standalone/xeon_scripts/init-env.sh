a09b5d8802f8145f16b112f2f3dc1f07d17ff0dd
Slava Krutelyov <slava77@cern.ch>
2022-11-08 16:58:56 -0800

    update sta init script to 12_6_0_pre4

--- RecoTracker/MkFitCore/standalone/xeon_scripts/init-env.sh
+++ RecoTracker/MkFitCore/standalone/xeon_scripts/init-env.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-source /cvmfs/cms.cern.ch/slc7_amd64_gcc10/lcg/root/6.24.07-f52350f4e0b802edeb9a2551a7d00b92/etc/profile.d/init.sh
-export TBB_GCC=/cvmfs/cms.cern.ch/slc7_amd64_gcc10/external/tbb/v2021.4.0-d0152ca29055e3a1bbf629673f6e97c4
-
+source /cvmfs/cms.cern.ch/slc7_amd64_gcc10/lcg/root/6.24.07-26a448a5bfafa671ac41ad592c8e7413/etc/profile.d/init.sh
+export TBB_GCC=/cvmfs/cms.cern.ch/slc7_amd64_gcc10/external/tbb/v2021.5.0-36aff7df349e0716374b1668ccd18e17
