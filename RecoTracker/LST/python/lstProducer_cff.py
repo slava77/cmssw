9f697fd3199a4dba3aaae09385c1924ae147784d
Slava Krutelyov <slava77@cern.ch>
2024-06-05 16:39:31 -0700

    put lstModulesDevESProducer on a dummy visible task to bypass the module creation

--- RecoTracker/LST/python/lstProducer_cff.py
+++ RecoTracker/LST/python/lstProducer_cff.py
@@ -7,3 +7,6 @@ from Configuration.ProcessModifiers.gpu_cff import gpu
 
 from RecoTracker.LST.lstModulesDevESProducer_cfi import lstModulesDevESProducer
 (~gpu).toModify(lstModulesDevESProducer.alpaka, backend = 'serial_sync')
+
+# not scheduled task to get the framework to hide the producer
+dummyLSTModulesDevESProducerTask = cms.Task(lstModulesDevESProducer)
