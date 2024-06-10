944652f36985a42d150edde23a4b47b970103a19
Manos Vourliotis <emmanouil.vourliotis@gmail.com>
2024-06-11 01:17:16 +0200

    Keep comment closer to the added task object
    Co-authored-by: Slava Krutelyov <slava77@gmail.com>

--- RecoTracker/LST/python/lstProducer_cff.py
+++ RecoTracker/LST/python/lstProducer_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoTracker.LST.lstProducer_cfi import lstProducer
 
-# not scheduled task to get the framework to hide the producer
 from RecoTracker.LST.lstModulesDevESProducer_cfi import lstModulesDevESProducer
+
+# not scheduled task to get the framework to hide the producer
 dummyLSTModulesDevESProducerTask = cms.Task(lstModulesDevESProducer)
