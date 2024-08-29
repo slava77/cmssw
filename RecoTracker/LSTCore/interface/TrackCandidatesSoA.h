3e98eb4e71fa9573ccff222ba0b9f18ebc44f809
Slava Krutelyov <slava77@cern.ch>
2024-08-28 20:49:20 -0700

    code checks

--- RecoTracker/LSTCore/interface/TrackCandidatesSoA.h
+++ RecoTracker/LSTCore/interface/TrackCandidatesSoA.h
@@ -9,21 +9,21 @@
 
 namespace lst {
   GENERATE_SOA_LAYOUT(TrackCandidatesSoALayout,
-                      SOA_COLUMN(short, trackCandidateType),                       // 4-T5 5-pT3 7-pT5 8-pLS
-                      SOA_COLUMN(unsigned int, directObjectIndices),               // direct indices to each type containers
-                      SOA_COLUMN(ArrayUx2, objectIndices),                         // tracklet and  triplet indices
-                      SOA_COLUMN(Params_pT5::ArrayU8xLayers, logicalLayers),       // 
-                      SOA_COLUMN(Params_pT5::ArrayUxHits, hitIndices),             // 
-                      SOA_COLUMN(int, pixelSeedIndex),                             // 
-                      SOA_COLUMN(Params_pT5::ArrayU16xLayers, lowerModuleIndices), // 
-                      SOA_COLUMN(FPX, centerX),                                    // 
-                      SOA_COLUMN(FPX, centerY),                                    // 
-                      SOA_COLUMN(FPX, radius),                                     // 
-                      SOA_SCALAR(unsigned int, nTrackCandidates),                  //
-                      SOA_SCALAR(unsigned int, nTrackCandidatespT3),               //
-                      SOA_SCALAR(unsigned int, nTrackCandidatespT5),               //
-                      SOA_SCALAR(unsigned int, nTrackCandidatespLS),               //
-                      SOA_SCALAR(unsigned int, nTrackCandidatesT5))                //
+                      SOA_COLUMN(short, trackCandidateType),                  // 4-T5 5-pT3 7-pT5 8-pLS
+                      SOA_COLUMN(unsigned int, directObjectIndices),          // direct indices to each type containers
+                      SOA_COLUMN(ArrayUx2, objectIndices),                    // tracklet and  triplet indices
+                      SOA_COLUMN(Params_pT5::ArrayU8xLayers, logicalLayers),  //
+                      SOA_COLUMN(Params_pT5::ArrayUxHits, hitIndices),        //
+                      SOA_COLUMN(int, pixelSeedIndex),                        //
+                      SOA_COLUMN(Params_pT5::ArrayU16xLayers, lowerModuleIndices),  //
+                      SOA_COLUMN(FPX, centerX),                                     //
+                      SOA_COLUMN(FPX, centerY),                                     //
+                      SOA_COLUMN(FPX, radius),                                      //
+                      SOA_SCALAR(unsigned int, nTrackCandidates),                   //
+                      SOA_SCALAR(unsigned int, nTrackCandidatespT3),                //
+                      SOA_SCALAR(unsigned int, nTrackCandidatespT5),                //
+                      SOA_SCALAR(unsigned int, nTrackCandidatespLS),                //
+                      SOA_SCALAR(unsigned int, nTrackCandidatesT5))                 //
 
   using TrackCandidatesSoA = TrackCandidatesSoALayout<>;
 
@@ -34,6 +34,6 @@ namespace lst {
     v.nTrackCandidatespT5() = 0;
     v.nTrackCandidatespLS() = 0;
   }
-  
+
 }  // namespace lst
 #endif
